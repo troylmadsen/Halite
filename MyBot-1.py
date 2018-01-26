74cfe693a587620bbf47600413f55f70377dd7b7
Troy Madsen <troylmadsen@gmail.com>
2018-01-26 02:48:18 -0500

    -Spelling fix

--- MyBot-1.py
+++ MyBot-1.py
@@ -129,7 +129,7 @@ while True:
             entities_by_distance = game_map.nearby_entities_by_distance(ship)
             entities_by_distance = OrderedDict(sorted(entities_by_distance.items(), key=lambda t: t[0]))
 
-            # Closes empty planets and their distancs
+            # Closest empty planets and their distances
             closest_empty_planets = [entities_by_distance[distance][0] for distance in entities_by_distance if isinstance(entities_by_distance[distance][0], hlt.entity.Planet) and not entities_by_distance[distance][0].is_owned()]
             closest_empty_planet_distances = [distance for distance in entities_by_distance if isinstance(entities_by_distance[distance][0], hlt.entity.Planet) and not entities_by_distance[distance][0].is_owned()]
 
